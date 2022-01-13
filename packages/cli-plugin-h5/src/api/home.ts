caeef3fd97a515a3333c072c64e75d996ab60a5f
lorettaLei <1052400576@qq.com>
2022-01-13 17:44:41 +0800

    'package.json'

--- packages/cli-plugin-h5/src/api/home.ts
+++ packages/cli-plugin-h5/src/api/home.ts
@@ -1,7 +1,7 @@
 import { postAction } from '@/utils/request'
 import { HomeRequestModel } from '@/model/home'
 const api = {
-  home: '/encourage/store/productDetail'
+  home: '/api'
 }
 export function getHomeDetail (params: HomeRequestModel):Promise<any> {
   return postAction(api.home, params)
